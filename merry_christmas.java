18f0a6130fc83325f6e658b744a86c1d2b6bf7e7
George <122010570+georgepata98@users.noreply.github.com>
2025-12-10 17:11:06 +0200

    Update merry_christmas.java

--- merry_christmas.java
+++ merry_christmas.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.lang.Thread;
+
 public class merry_christmas
 {
   public static void main(String[] args)
@@ -9,36 +12,67 @@ public class merry_christmas
     String YELLOW = "\u001B[33m";
     String RED = "\u001B[31m";
 
-    System.out.println(YELLOW + "       2026" + RESET);
-
-    for(int i=0; i<10; i++)
+    try
     {
-      for(int j=0; j<20; j++)
+      for(int i=0; i<50; i++)
+      { System.out.println(); }
+
+      for(int k=0; k<2; k++)
       {
-        if(j>=9-i && j<= 9+i)
+        System.out.println(YELLOW + "       2026" + RESET);
+
+        for(int i=0; i<10; i++)
         {
-          if((i==2 && j==8) || (i==3 && j==11) || (i==5 && j==4) || (i==5 && j==8) || (i==6 && j==11) || (i==7 && j==7) || (i==7 && j==14) || (i==8 && j==3) || (i==8 && j==9) || (i==9 && j==5) || (i==9 && j==16)) { System.out.print(RED + "o" + RESET); }
+          for(int j=0; j<20; j++)
+          {
+            if(j>=9-i && j<= 9+i)
+            {
+              if((i==2 && j==8) || (i==3 && j==11) || (i==5 && j==4) || (i==5 && j==8) || (i==6 && j==11) || (i==7 && j==7) || (i==7 && j==14) || (i==8 && j==3) || (i==8 && j==9) || (i==9 && j==5) || (i==9 && j==16)) { System.out.print(RED + "o" + RESET); }
 
-          else { System.out.print(GREEN + "*" + RESET); }
+              else { System.out.print(GREEN + "*" + RESET); }
+            }
+            if(j<9-i || j>9+i) { System.out.print(" "); }
+          }
+          System.out.println();
         }
-        if(j<9-i || j>9+i) { System.out.print(" "); }
-      }
-      System.out.println();
-    }
 
-    for(int i=0; i<3; i++)
+        for(int i=0; i<3; i++)
+        {
+          for(int j=0; j<11; j++)
+          {
+            if(j<8) { System.out.print(" "); }
+            if(j==8 || j==10) { System.out.print(BROWN + "H" + RESET); }
+            if(j==9) { System.out.print(BROWN + "M" + RESET); }
+          }
+          System.out.println();
+        }
+
+        if(k==0)
+        {
+          System.out.println();
+          System.out.println(BLUE + "  MERRY" + RED + " CHRISTMAS !" + RESET);
+          System.out.println(BLUE + " AND HAPPY NEW YEAR," + YELLOW + " 2026" + RESET);
+        }
+
+        if(k==1)
+        {
+          System.out.println();
+          System.out.println(RED + "  MERRY" + BLUE + " CHRISTMAS !" + RESET);
+          System.out.println(RED + " AND HAPPY NEW YEAR," + YELLOW + " 2026" + RESET);
+        }
+
+        if(k<1)
+        {
+          Thread.sleep(3000);  // sleep for 3000 milliseconds
+
+          for(int i=0; i<50; i++)
+          { System.out.println(); }  // pe post de clear screen
+        }
+      }
+    }
+    catch(InterruptedException e)
     {
-      for(int j=0; j<11; j++)
-      {
-        if(j<8) { System.out.print(" "); }
-        if(j==8 || j==10) { System.out.print(BROWN + "H" + RESET); }
-        if(j==9) { System.out.print(BROWN + "M" + RESET); }
-      }
-      System.out.println();
+      System.out.println("got interrupted!");
     }
-
-    System.out.println();
-    System.out.println(BLUE + "  MERRY CHRISTMAS !" + RESET);
-    System.out.println(BLUE + " AND HAPPY NEW YEAR, 2026" + RESET);
   }
 }
